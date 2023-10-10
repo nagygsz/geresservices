3ea7ff3447006beebd4dd1c8360f305fc2e57dd5
nagygsz <imotep1526@gmail.com>
2023-10-10 14:29:22 +0200

    Add fraud module

---
+++ fraud/src/main/java/eu/nagygsz/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package eu.nagygsz.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/eu/nagygsz/fraud/FraudController.java
@@ -0,0 +1,23 @@
+package eu.nagygsz.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@AllArgsConstructor
+public class FraudController {
+
+    private final FraudCheckService fraudCheckService;
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(
+            @PathVariable("customerId") Integer customerID
+    ) {
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}

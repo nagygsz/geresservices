40ab0bc983e9e6ba6df993bdad069f7fb5f9f6dc
nagygsz <imotep1526@gmail.com>
2023-10-10 15:00:46 +0200

    Communicate between microservices via RestTemplate

---
+++ customer/src/main/java/eu/nagygsz/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package eu.nagygsz.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/eu/nagygsz/fraud/FraudController.java
+++ fraud/src/main/java/eu/nagygsz/fraud/FraudController.java
@@ -1,14 +1,16 @@
 package eu.nagygsz.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -18,6 +20,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerID
     ) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }

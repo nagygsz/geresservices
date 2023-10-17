df97f046729cc8d00fc407ebb3e2a5c4e81b394b
nagygsz <imotep1526@gmail.com>
2023-10-17 16:37:23 +0200

    Add sleuth and fix version issues

--- customer/src/main/java/eu/nagygsz/customer/Customer.java
+++ customer/src/main/java/eu/nagygsz/customer/Customer.java
@@ -1,10 +1,11 @@
 package eu.nagygsz.customer;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
--- notification/src/main/java/eu/nagygsz/notification/Notification.java
+++ notification/src/main/java/eu/nagygsz/notification/Notification.java
@@ -1,8 +1,8 @@
 package eu.nagygsz.notification;
 
-import jakarta.persistence.*;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
